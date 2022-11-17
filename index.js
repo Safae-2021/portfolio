0aa571e13a944ed3478665d8e5c735d6756e75d4
Safae-2021 <safaetahere.st@gmail.com>
2022-11-17 11:00:58 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".img-1").slideDown("slow");
+
+    // $(".img-1").slideOut("3000")
+    // $(".img-1").fadeIn();
+    // $(".img-1").fadeIn("slow");
+    // $(".img-1").addClass("img-2")  
+
+    // typing animation
+    var typed = new Typed(".typing", {
+        strings:["Developer","Trainee"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    })
+})
+
+  
+
+
+
+
